6761b00fcd60ceb1878d675fe8815fe872cd2816
rehana-220625 <n220625@rguktn.ac.in>
2026-03-24 08:57:32 -0700

    Update alert.tsx

--- client/src/components/ui/alert.tsx
+++ client/src/components/ui/alert.tsx
@@ -3,6 +3,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
+
 const alertVariants = cva(
   "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
   {
@@ -43,6 +44,7 @@ const AlertTitle = React.forwardRef<
   />
 ))
 AlertTitle.displayName = "AlertTitle"
+
 
 const AlertDescription = React.forwardRef<
   HTMLParagraphElement,
